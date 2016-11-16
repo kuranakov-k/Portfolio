ff4a2a2552e3d5ad8a013642b012de7eb2f7233c
Konstantin Kuranakov <k.kuranakov@gmail.com>
2016-11-16 13:52:15 +0600

    added dobrbobr

---
+++ templates/dobrbobr/js/custom/custom.js
@@ -0,0 +1,18 @@
+$(function() {
+
+		$('#slider-reviews').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true
+    });
+
+    $('#slider-questions').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true
+    });
+
+});
